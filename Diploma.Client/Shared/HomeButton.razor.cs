e0e515a656179e65756f84a2500b8d84ef499ee4
Dastan Abishev <abish.ev@mail.ru>
2023-05-21 21:37:16 +0200

    - Added Home button

---
+++ Diploma.Client/Shared/HomeButton.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Diploma.Client.Shared
+{
+    public partial class HomeButton
+    {
+        private void NavigateToHomePage()
+        {
+            NavigationManager.NavigateTo("");
+        }
+    }
+}
